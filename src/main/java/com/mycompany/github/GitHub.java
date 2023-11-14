5144beff4797295fa56ce0ad4c1be8c7f0434800
elqli <elqli@MSI>
2023-11-13 18:14:18 -0600

    probando mi primer repositorio en github virtual java

--- src/main/java/com/mycompany/github/GitHub.java
+++ src/main/java/com/mycompany/github/GitHub.java
@@ -12,6 +12,6 @@ package com.mycompany.github;
 public class GitHub {
 
     public static void main(String[] args) {
-        System.out.println("github hola");
+        System.out.println("github hola d");
     }
 }
